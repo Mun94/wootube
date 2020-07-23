e86c025710369ab0ef6a6cb2c8b3051ab3f03f50
Mun94 <seongwoo720@gmail.com>
2020-07-23 15:04:39 +0900

    200723 | DONE | router 및 미들웨어 적용

---
+++ app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import userRouter from './routers/userRouter.js';
+import videoRouter from './routers/videoRouter.js';
+import globalRouter from './routers/globalRouter.js';
+
+const app = express();
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(helmet());
+app.use(morgan("dev"));
+
+app.get("/", globalRouter);
+app.get("/user", userRouter)
+app.use("/video", videoRouter);
+
+export default app;
