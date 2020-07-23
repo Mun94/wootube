341a71b8bd23be4d40d6e4a1c74dcf97ce57a0ba
Mun94 <seongwoo720@gmail.com>
2020-07-23 15:45:09 +0900

    200723 | ING | MVC 패턴

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ import bodyParser from "body-parser";
 import userRouter from './routers/userRouter.js';
 import videoRouter from './routers/videoRouter.js';
 import globalRouter from './routers/globalRouter.js';
+import routes from './routes.js';
 
 const app = express();
 
@@ -15,8 +16,8 @@ app.use(bodyParser.urlencoded({extended : true}));
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.get("/", globalRouter);
-app.get("/user", userRouter)
-app.use("/video", videoRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
