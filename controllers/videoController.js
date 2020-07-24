4c3a072bb4b9ff17f3d564f1f499b4230bd75b18
Mun94 <seongwoo720@gmail.com>
2020-07-24 14:43:32 +0900

    200724 | DONE | More controllers

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,12 +1,19 @@
 import { videos } from '../db.js';
+import routes from '../routes.js';
 
 export const home = (req, res) => res.render("home", { pageTitle: "Home", videos});
 export const search = (req, res) => {
     const {query : {term : searchingBy}} = req;
     return res.render("search", { pageTitle: "Search", searchingBy, videos})
 };
-export const upload = (req, res) =>
+export const getUpload = (req, res) =>
   res.render("upload", { pageTitle: "Upload" });
+export const postUpload = (req, res) => {
+  const {
+    body : {file, title, description}
+  } = req;
+  res.redirect(routes.videoDetail(324393));
+};
 export const videoDetail = (req, res) =>
   res.render("videoDetail", { pageTitle: "Video Detail" });
 export const editVideo = (req, res) =>
