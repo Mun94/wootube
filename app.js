ac3cfbb62c1a12df357706c4906c589a10c3a4f3
Mun94 <seongwoo720@gmail.com>
2020-07-31 10:48:39 +0900

    200731 | DONE | Sessions on express

--- app.js
+++ app.js
@@ -4,31 +4,34 @@ import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import passport from "passport";
-import userRouter from "./routers/userRouter.js";
-import videoRouter from "./routers/videoRouter.js";
-import globalRouter from "./routers/globalRouter.js";
-import routes from "./routes.js";
-import { localsMiddleware } from "./middlewares.js";
-
+import session from "express-session";
+import { localsMiddleware } from "./middlewares";
+import routes from "./routes";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
 import "./passport";
-
 const app = express();
-
-app.set("view engine", "pug");
-
 app.use(helmet());
+app.set("view engine", "pug");
+app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("static"));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
-app.use(localsMiddleware);
-app.use("/uploads", express.static("uploads"));
-app.use("/static", express.static("static"));
+app.use(
+  session({
+    secret: process.env.JWT_SECRET,
+    resave: true,
+    saveUninitialized: false,
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.use(localsMiddleware);
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
 app.use(routes.videos, videoRouter);
-
 export default app;
