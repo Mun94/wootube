b32ae9ebb7049d52ae15e31f4b4f4702c881ddbe
Mun94 <seongwoo720@gmail.com>
2020-08-01 11:50:55 +0900

    200801 | DONE | Edit user profile

--- controllers/userController.js
+++ controllers/userController.js
@@ -75,5 +75,22 @@ export const userDetail = async (req, res) => {
     res.redirect(routes.home);
   }
 };
-export const editProfile = (req, res) => res.render("editProfile");
+export const getEditProfile = (req, res) =>
+  res.render("editProfile", { pageTitle: "Edit Profile" });
+export const postEditProfile = async (req, res) => {
+  const {
+    body: { name, email },
+    file,
+  } = req;
+  try {
+    await User.findByIdAndUpdate(req.user.id, {
+      name,
+      email,
+      avatarUrl: file ? file.path : req.user.avatarUrl,
+    });
+    res.redirect(routes.me);
+  } catch (e) {
+    res.render("editProfile", { pageTitle: "Edit Profile" });
+  }
+};
 export const changePassword = (req, res) => res.render("changePassword");
--- middlewares.js
+++ middlewares.js
@@ -2,12 +2,12 @@ import routes from "./routes.js";
 import multer from "multer";
 
 const multerVideo = multer({ dest: "uploads/videos/" });
+const multerAvatar = multer({ dest: "uploads/avatars/" });
 
 export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = "WooTube";
   res.locals.routes = routes;
   res.locals.loggedUser = req.user || null;
-  console.log(req.user);
   next();
 };
 
@@ -28,3 +28,4 @@ export const onlyPrivate = (req, res, next) => {
 };
 
 export const uploadVideo = multerVideo.single("videoFile");
+export const uploadAvatar = multerAvatar.single("avatar");
