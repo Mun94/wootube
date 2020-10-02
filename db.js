d63de51a289fa80b8f7df443ec144ed7cd76732d
Mun94 <seongwoo720@gmail.com>
2020-10-02 15:58:11 +0900

    201002 | DONE | Home controller

---
+++ db.js
@@ -0,0 +1,14 @@
+export const videos = [
+  {
+    id: 324393,
+    title: "video awesome",
+    description: "This is something i love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 121212,
+      name: "mun",
+      email: "seongwoo@gmail.com",
+    },
+  },
+];
