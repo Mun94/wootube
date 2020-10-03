15657a3f379026e1c91e445d433696f21eae4138
Mun94 <seongwoo720@gmail.com>
2020-10-03 12:31:37 +0900

    201003 | DONE | Installing ESLint

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -3,7 +3,7 @@ import Video from "../schemas/video.js";
 
 export const home = async (req, res) => {
   try {
-    const videos = await Video.find({});
+    const videos = await Video.find({}).sort({ _id: -1 });
     res.render("home", { pageTitle: "Home", videos });
   } catch (e) {
     res.render("home", { pageTitle: "Home", videos: [] });
