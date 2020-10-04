8697d6d323a1cb60e2473705bbf37742a007c6f5
Mun94 <seongwoo720@gmail.com>
2020-10-05 03:28:11 +0900

    201005 | DONE | Total time and current time

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -3,6 +3,8 @@ const videoPlayer = document.querySelector("#jsVideoPlayer video");
 const playBtn = document.getElementById("jsPlayButton");
 const volumeBtn = document.getElementById("jsVolumeBtn");
 const fullScrnBtn = document.getElementById("jsFullScreen");
+const currentTime = document.getElementById("currentTime");
+const totalTime = document.getElementById("totalTime");
 
 const handlePlayClick = () => {
   if (videoPlayer.paused) {
@@ -25,22 +27,51 @@ const handleVolumeClick = () => {
 };
 
 const goFullScreen = () => {
-  videoContainer.RequestFullscreen();
+  videoContainer.requestFullscreen();
   fullScrnBtn.innerHTML = '<i class="fas fa-compress"></i>';
   fullScrnBtn.removeEventListener("click", goFullScreen);
   fullScrnBtn.addEventListener("click", exitFullscreen);
 };
 
 const exitFullscreen = () => {
-  document.ExitFullscreen();
+  document.exitFullscreen();
   fullScrnBtn.innerHTML = '<i class="fas fa-expand"></i>';
   fullScrnBtn.addEventListener("click", goFullScreen);
+};
+
+const formatDate = (seconds) => {
+  const secondsNumber = parseInt(seconds);
+  let hours = Math.floor(secondsNumber / 3600);
+  let minutes = Math.floor((secondsNumber - hours * 3600) / 60);
+  let totalSeconds = secondsNumber - hours * 3600 - minutes * 60;
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (totalSeconds < 10) {
+    totalSeconds = `0${totalSeconds}`;
+  }
+
+  return `${hours}:${minutes}:${totalSeconds}`;
+};
+
+const getCurrentTime = () => {
+  currentTime.innerHTML = formatDate(videoPlayer.currentTime);
+};
+
+const setTotalTime = () => {
+  totalTime.innerHTML = formatDate(videoPlayer.duration);
+  setInterval(getCurrentTime, 1000);
 };
 
 const init = () => {
   playBtn.addEventListener("click", handlePlayClick);
   volumeBtn.addEventListener("click", handleVolumeClick);
   fullScrnBtn.addEventListener("click", goFullScreen);
+  videoPlayer.addEventListener("loadedmetadata", setTotalTime);
 };
 
 if (videoContainer) {
