cf7d0a066307155bd19e7b9dd17cdf822bafae6c
Mun94 <seongwoo720@gmail.com>
2020-10-04 12:06:28 +0900

    201004 | DONE | Edit user profile

--- middlewares.js
+++ middlewares.js
@@ -2,7 +2,7 @@ import routes from "./routes.js";
 import multer from "multer";
 
 const multerVideo = multer({ dest: "uploads/videos/" });
-
+const multerAvatar = multer({ dest: "uploads/avatars/" });
 export const localsMiddleware = (req, res, next) => (
   (res.locals.siteName = "WooTube"),
   (res.locals.routes = routes),
@@ -27,3 +27,4 @@ export const onlyPrivate = (req, res, next) => {
 };
 
 export const uploadVideo = multerVideo.single("videoFile");
+export const uploadAvatar = multerAvatar.single("avatar");
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -2,14 +2,16 @@ import express from "express";
 import routes from "../routes.js";
 import {
   userDetail,
-  editProfile,
+  getEditProfile,
+  postEditProfile,
   changePassword,
 } from "../controllers/userController.js";
-import { onlyPrivate } from "../middlewares.js";
+import { onlyPrivate, uploadAvatar } from "../middlewares.js";
 
 const userRouter = express.Router();
 
-userRouter.get(routes.editProfile, onlyPrivate, editProfile);
+userRouter.get(routes.editProfile, onlyPrivate, getEditProfile);
+userRouter.post(routes.editProfile, onlyPrivate, uploadAvatar, postEditProfile);
 userRouter.get(routes.changePassword, onlyPrivate, changePassword);
 userRouter.get(routes.userDetail(), userDetail);
 
