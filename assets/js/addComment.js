9b7cdd502d991d2f4934d0a06f49901b58207b1d
Mun94 <seongwoo720@gmail.com>
2020-10-06 13:14:41 +0900

    201006 | DONE | API adding a comment

---
+++ assets/js/addComment.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+const addCommentForm = document.getElementById("jsAddComment");
+const commentList = document.getElementById("jsCommentList");
+const commentNumber = document.getElementById("jsCommentNumber");
+
+const increaseNumber = () => {
+  commentNumber.innerHTML = parseInt(commentNumber.innerHTML) + 1;
+};
+
+const addComment = (comment) => {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  span.innerHTML = comment;
+  li.append(span);
+  commentList.prepend(li);
+  increaseNumber();
+};
+
+const sendComment = async (comment) => {
+  const videoId = window.location.href.split("/videos/")[1];
+  const response = await axios.post(`/api/${videoId}/comment`, { comment });
+  if (response.status === 200) {
+    addComment(comment);
+  }
+};
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  console.log("eeee", e.target.value);
+  const commentInput = addCommentForm.querySelector("input");
+  const comment = commentInput.value;
+  sendComment(comment);
+  commentInput.value = "";
+};
+
+const init = () => {
+  addCommentForm.addEventListener("submit", handleSubmit);
+};
+
+if (addCommentForm) {
+  init();
+}
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,5 +1,6 @@
 import routes from "../routes.js";
 import Video from "../schemas/video.js";
+import Comment from "../schemas/comment.js";
 
 export const home = async (req, res) => {
   try {
@@ -47,7 +48,9 @@ export const videoDetail = async (req, res) => {
     params: { id },
   } = req;
   try {
-    const video = await Video.findById(id).populate("creator");
+    const video = await Video.findById(id)
+      .populate("creator")
+      .populate("comments");
     res.render("videoDetail", { pageTitle: video.title, video });
   } catch (e) {
     res.redirect(routes.home);
@@ -108,6 +111,27 @@ export const postRegisterView = async (req, res) => {
     video.views += 1;
     video.save();
     res.status(200);
+  } catch (e) {
+    res.status(400);
+  } finally {
+    res.end();
+  }
+};
+
+export const postAddComment = async (req, res) => {
+  const {
+    params: { id },
+    body: { comment },
+    user,
+  } = req;
+  try {
+    const video = await Video.findById(id);
+    const newComment = await Comment.create({
+      text: comment,
+      creator: user.id,
+    });
+    video.comments.push(newComment.id);
+    video.save();
   } catch (e) {
     res.status(400);
   } finally {
