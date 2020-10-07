0ae5b09cb0bd8fbb93bdb4a894161f43b1aa6440
Mun94 <seongwoo720@gmail.com>
2020-10-07 23:24:04 +0900

    201007 | DONE |Heroku deploy

--- src/passport.js
+++ src/passport.js
@@ -12,7 +12,7 @@ passport.use(
     {
       clientID: process.env.GH_ID,
       clientSecret: process.env.GH_SECRET,
-      callbackURL: `http://localhost:4000${routes.githubCallback}`,
+      callbackURL: `https://hidden-earth-27978.herokuapp.com/${routes.githubCallback}`,
     },
     githubLoginCallback
   )
